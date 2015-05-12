78c4b857ece94ce7c9bd6adbdcd9eb5837314d6e
Eli Duke <elimduke@gmail.com>
2015-05-11 23:13:55 -0700

    still tweaking the new list form js.

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -2,22 +2,49 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 
-  $("#bullets").click(function(){
+  $("#list_title").focus();
 
+  $("#bullets").click(function(){
     $("#bullets").addClass("selected");
     $("#numbers").removeClass("selected");
-    $("#list ol").replaceWith($('<ul>'+$("#list ol").html()+'</ul>'));
-
+    $("#list ol").replaceWith($('<ul id="list_items">'+$("#list ol").html()+'</ul>'));
     return false;
   });
 
   $("#numbers").click(function(){
-
     $("#bullets").removeClass("selected");
     $("#numbers").addClass("selected");
-    $("#list ul").replaceWith($('<ol>'+$("#list ul").html()+'</ol>'));
-
+    $("#list ul").replaceWith($('<ol id="list_items">'+$("#list ul").html()+'</ol>'));
     return false;
+  });
+
+  $("#comments_on").click(function(){
+    $("#comments_on").addClass("selected");
+    $("#comments_off").removeClass("selected");
+    return false;
+  });
+
+  $("#comments_off").click(function(){
+    $("#comments_off").addClass("selected");
+    $("#comments_on").removeClass("selected");
+    return false;
+  });
+
+  $("form").keydown(function (e) {
+    if (e.keyCode == 13) {
+      return false;
+    }
+  });
+
+  $("form #list_items li:last").keydown(function (e) {
+    if (e.keyCode == 13) {
+      inputs = $("#list_items li");
+      new_input = '<li><input class="form-control input-lg" placeholder="Type and Press ENTER" type="text" name="list[item]" id="list_item_' + (inputs.length + 1) + '" /></li>';
+      $("#list_items").append(new_input);
+      $("#list_item_" + inputs.length).attr("placeholder", "Item " + inputs.length);
+      $("#list_item_" + (inputs.length + 1)).focus();
+      return false;
+    }
   });
 
 });
