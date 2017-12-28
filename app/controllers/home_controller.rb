8cbbdd6bb1b5344368b4548ad15c30600eb897ad
Eli Duke <elimduke@gmail.com>
2017-12-28 03:14:23 -1000

    Only public lists for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @title = "Pointless List-Making at its Finest"
-    @lists_new = List.order(created_at: :desc).limit(14)
-    @lists_activity = List.where(id: Comment.order(created_at: :desc).limit(14).map(&:list_id).uniq)
-    @lists_popular = List.order(hits: :desc).limit(14)
+    @lists_new = List.publics.order(created_at: :desc).limit(14)
+    @lists_activity = List.publics.where(id: Comment.order(created_at: :desc).limit(14).map(&:list_id).uniq)
+    @lists_popular = List.publics.order(hits: :desc).limit(14)
   end
 
   def about
