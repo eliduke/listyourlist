6e70ca00d0758c9ef1af8338bf5a6723cfc58832
Eli Duke <elimduke@gmail.com>
2017-12-31 14:47:05 -1000

    A little bit more list form clean-up stuff

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,5 +1,8 @@
 function flexText(element) {
+  // Grabs existing textarea height
   textAreaHeight = $(element).css("height").split("px")[0]
+
+  // Resizes if the scroll height exceeds existing textarea height
   if (element.scrollHeight > textAreaHeight) {
     $(element).css('height', element.scrollHeight + 'px');
   }
@@ -7,19 +10,24 @@ function flexText(element) {
 
 function flexTextAll() {
   $("textarea.flex-text").each(function(index, element) {
+    // Resizes all existing textareas with current text
     flexText(element);
 
+    // Sets listener on all existing textareas for future text increases
     $(element).on('input', function() {
       flexText(this);
     });
   });
 }
 
-function setCocoonCallback() {
-  $(".links").on('cocoon:after-insert', function(e, insertedItem) {
-    textArea = insertedItem.find(".js-list-item");
+function setListenersForAddingNewItem() {
+  $(".links").on('cocoon:after-insert', function(e, newItemFields) {
+    // Sets correctly incremented placeholder text and moves focus
+    textArea = newItemFields.find("textarea");
     textArea.attr("placeholder", "List Item " + $(".js-list-item").length);
     textArea.focus();
+
+    // Sets flexText listener on new textarea
     textArea.on('input', function() {
       flexText(this);
     });
@@ -27,17 +35,8 @@ function setCocoonCallback() {
 }
 
 $(function(){
-  setCocoonCallback();
   flexTextAll();
-
-  $("textarea.flex-text").focus(function() {
-    // hack for putting cursor at the end when clicking or hitting enter
-    // doesn't work for tabbing in for some reason. TODO figure that out
-    input = $(this);
-    var tmpStr = input.val();
-    input.val('');
-    input.val(tmpStr);
-  });
+  setListenersForAddingNewItem();
 
   $(".js-switch-list-type").change(function() {
     existingItems = []
@@ -56,8 +55,10 @@ $(function(){
       $(this).val(existingItems[index]);
     });
 
+    // Switching out the entire list contents removes all listeners
+    // This resets all the necessary listeners
     flexTextAll();
-    setCocoonCallback();
+    setListenersForAddingNewItem();
   });
 
   $("form.js-list-form").keydown(function (e) {
@@ -84,14 +85,47 @@ $(function(){
     }
   });
 
+
+  // Moves cursor to the end of a flex text when clicking or hitting enter
+  // This doesn't work for tabbing in for some reason. TODO figure that out
+  $("textarea.flex-text").focus(function() {
+    input = $(this);
+    var tmpStr = input.val();
+    input.val('');
+    input.val(tmpStr);
+  });
+
+  // Highlights all text in a field when tabbing into it because the above
+  // function isn't working for tabbing. Figured this was better than nothing.
   $("form.js-list-form").keyup(function (e) {
     if (e.which == 9) {
       $(document.activeElement).select();
     }
   });
 
+  // Toggles form help info 🤔
   $(".js-form-help").click(function(e) {
     e.preventDefault();
     $(".form-help").toggle();
   });
 });
+
+// TODO I want smarter item delete confirmation dialogues.
+// This was an attempt but didn't work quite well
+//
+// function setItemDeleteConfirmation(element) {
+//   $(element).click(function() {
+//     if ($(element).siblings().find("textarea").val().length > 0) {
+//       response = confirm("Are you sure you want to delete this item?");
+//       if (response == false) {
+//         return false;
+//       }
+//     }
+//   });
+// }
+//
+// function setItemDeleteConfirmationAll() {
+//   $("a.remove_fields").each(function(index, element) {
+//     setItemDeleteConfirmation(element);
+//   });
+// }
